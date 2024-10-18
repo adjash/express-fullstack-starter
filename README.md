a898a20a45ab35362ebcbee56294a82cb7f3de9a
adjash <adam.hardie13@gmail.com>
2024-10-18 14:50:38 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Full-Stack playground
+
+Minimal full-stack express app setup.
+Very basic chat app functionality.
+
+TODO:
+Add React Frontend
+Configure API endpoints/folder structure
+Integrate with sqlite
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // Enable dark mode via a 'class' strategy
+  content: ["./*.html", "./public/**/*.html", "./views/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
